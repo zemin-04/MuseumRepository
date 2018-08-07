e14a6b49bba1e92eb47f501173a806cbec7b0b98
SkipCloud <zemin_04@163.com>
2018-08-07 22:11:12 +0800

    添加遗漏的文物业务处理类

---
+++ museum/src/main/java/com/zhongda/museum/service/impl/CulturalrelicsServiceImpl.java
@@ -0,0 +1,10 @@
+package com.zhongda.museum.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.zhongda.museum.service.CulturalrelicsService;
+
+@Service
+public class CulturalrelicsServiceImpl implements CulturalrelicsService {
+
+}
